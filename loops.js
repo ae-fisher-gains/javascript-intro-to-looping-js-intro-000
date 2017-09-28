64d1e03926bd3267277f3f47cecae7fc3d6a4c42
Adam Fisher <ae.fisher.gains@gmail.com>
2017-09-28 23:27:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop (array) {
+  var array = []
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+      array.push "I am 1 strange loop."
+    }
+    else if (i < 25) {
+      array.push `"I am ${i} strange loops."`
+    }
+    else if (i == 25) {
+      break;
+    }
+  }
+  return array
+}
